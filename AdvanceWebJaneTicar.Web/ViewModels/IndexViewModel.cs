8a854e0c3592037e717aa3b5ca74f5d58e47f4a8
Shayne Maravillo <yashnedobasa@gmail.com>
2019-01-09 15:23:18 +0800

    advanceWEbJaneTicar

---
+++ AdvanceWebJaneTicar.Web/ViewModels/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AdvanceWebJaneTicar.Web.Infrastructures.Data.Helpers;
+using AdvanceWebJaneTicar.Web.Infrastructures.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvanceWebJaneTicar.Web.ViewModels
+{
+    public class IndexViewModel
+    {
+        public Page<User> Users { get; set; }
+    }
+}
